297fb6aef2fe6c61b35d54f0d1436d4d1ba0c44d
zxs <26291844@qq.com>
2015-07-13 20:53:43 +0800

    20150713

---
+++ 出纳管理系统/出纳管理系统/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace 出纳管理系统
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ribbonControl = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.rgbiSkins = new DevExpress.XtraBars.RibbonGalleryBarItem();
+            this.homeRibbonPage = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.fileRibbonPageGroup = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.skinsRibbonPageGroup = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.exitRibbonPageGroup = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.helpRibbonPage = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.helpRibbonPageGroup = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbonControl
+            // 
+            this.ribbonControl.ApplicationButtonText = null;
+            this.ribbonControl.ExpandCollapseItem.Id = 0;
+            this.ribbonControl.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl.ExpandCollapseItem,
+            this.rgbiSkins});
+            this.ribbonControl.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl.MaxItemId = 1;
+            this.ribbonControl.Name = "ribbonControl";
+            this.ribbonControl.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.homeRibbonPage,
+            this.helpRibbonPage});
+            this.ribbonControl.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonControlStyle.Office2010;
+            this.ribbonControl.Size = new System.Drawing.Size(1100, 147);
+            // 
+            // rgbiSkins
+            // 
+            this.rgbiSkins.Caption = "Skins";
+            // 
+            // 
+            // 
+            this.rgbiSkins.Gallery.AllowHoverImages = true;
+            this.rgbiSkins.Gallery.Appearance.ItemCaptionAppearance.Normal.Options.UseFont = true;
+            this.rgbiSkins.Gallery.Appearance.ItemCaptionAppearance.Normal.Options.UseTextOptions = true;
+            this.rgbiSkins.Gallery.Appearance.ItemCaptionAppearance.Normal.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.rgbiSkins.Gallery.ColumnCount = 4;
+            this.rgbiSkins.Gallery.FixedHoverImageSize = false;
+            this.rgbiSkins.Gallery.ImageSize = new System.Drawing.Size(32, 17);
+            this.rgbiSkins.Gallery.ItemImageLocation = DevExpress.Utils.Locations.Top;
+            this.rgbiSkins.Gallery.RowCount = 4;
+            this.rgbiSkins.Id = 60;
+            this.rgbiSkins.Name = "rgbiSkins";
+            // 
+            // homeRibbonPage
+            // 
+            this.homeRibbonPage.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.fileRibbonPageGroup,
+            this.skinsRibbonPageGroup,
+            this.exitRibbonPageGroup});
+            this.homeRibbonPage.Name = "homeRibbonPage";
+            this.homeRibbonPage.Text = "Home";
+            // 
+            // fileRibbonPageGroup
+            // 
+            this.fileRibbonPageGroup.Name = "fileRibbonPageGroup";
+            this.fileRibbonPageGroup.Text = "File";
+            // 
+            // skinsRibbonPageGroup
+            // 
+            this.skinsRibbonPageGroup.ItemLinks.Add(this.rgbiSkins);
+            this.skinsRibbonPageGroup.Name = "skinsRibbonPageGroup";
+            this.skinsRibbonPageGroup.ShowCaptionButton = false;
+            this.skinsRibbonPageGroup.Text = "Skins";
+            // 
+            // exitRibbonPageGroup
+            // 
+            this.exitRibbonPageGroup.Name = "exitRibbonPageGroup";
+            this.exitRibbonPageGroup.Text = "Exit";
+            // 
+            // helpRibbonPage
+            // 
+            this.helpRibbonPage.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.helpRibbonPageGroup});
+            this.helpRibbonPage.Name = "helpRibbonPage";
+            this.helpRibbonPage.Text = "Help";
+            // 
+            // helpRibbonPageGroup
+            // 
+            this.helpRibbonPageGroup.Name = "helpRibbonPageGroup";
+            this.helpRibbonPageGroup.Text = "Help";
+            // 
+            // Form1
+            // 
+            this.AllowFormGlass = DevExpress.Utils.DefaultBoolean.False;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 696);
+            this.Controls.Add(this.ribbonControl);
+            this.Name = "Form1";
+            this.Ribbon = this.ribbonControl;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl;
+        private DevExpress.XtraBars.RibbonGalleryBarItem rgbiSkins;
+        private DevExpress.XtraBars.Ribbon.RibbonPage homeRibbonPage;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup fileRibbonPageGroup;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup skinsRibbonPageGroup;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup exitRibbonPageGroup;
+        private DevExpress.XtraBars.Ribbon.RibbonPage helpRibbonPage;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup helpRibbonPageGroup;
+
+    }
+}
